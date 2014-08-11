c7d5699a81693c8f66c06c16e27f34e4da8dbc58
jcejas <cejasjulio@gmail.com>
2014-08-11 15:09:41 -0430

    prueba

---
+++ poc-service/service-persistence/civil-jpa-persistence/src/main/java/com/mijao/poc/persistence/civil/dao/entities/SarcActa.java
@@ -0,0 +1,21 @@
+package com.mijao.poc.persistence.civil.dao.entities;
+
+import java.sql.Timestamp;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@javax.persistence.Table(name = "", schema = "", catalog = "")
+@Entity
+public class SarcActa implements Serializable {
+
+	@Id
+	private int id;
+    
+    private String number;
+    private String tipo;
+    private String fechaRegistro;
+    
+    
+    
+    
+}
